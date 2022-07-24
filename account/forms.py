f223ae9ab61a7cab60aecb04be9b12c1c718ea49
Mohan Ganta <85378086+Mohan-Ganta@users.noreply.github.com>
2022-07-25 00:01:55 +0530

    Add files via upload

---
+++ account/forms.py
@@ -0,0 +1,75 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import User
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        widget= forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+
+
+class SignUpForm(UserCreationForm):
+    firstname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    lastname = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    email = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    fulladdress = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control"
+            }
+        )
+    )
+    # image = forms.FileField()
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'is_admin', 'is_employee', 'is_customer','firstname','lastname','fulladdress',)
